2c5ae36f907554cf9f7dd416e63f8ec760979bf6
JINSAN NOH <zingoworks@gmail.com>
2019-09-27 01:20:43 +0900

    Add solutions

--- java-algorithm/src/main/java/leetcode/MiddleOfLinkedList.java
+++ java-algorithm/src/main/java/leetcode/MiddleOfLinkedList.java
@@ -20,6 +20,36 @@ public class MiddleOfLinkedList {
         return head;
     }
 
+    // just 1 iteration with 'walker & runner'
+    
+    public ListNode middleNode2(ListNode head) {
+        ListNode walker = head;
+        ListNode runner = head;
+
+        while(runner != null) {
+            runner = runner.next;
+
+            if(runner != null) {
+                walker = walker.next;
+                runner = runner.next;
+            }
+        }
+
+        return walker;
+    }
+
+    public ListNode middleNode3(ListNode head) {
+        ListNode walker = head;
+        ListNode runner = head;
+
+        while(runner != null && runner.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+        }
+
+        return walker;
+    }
+
     public class ListNode {
         int val;
         ListNode next;
