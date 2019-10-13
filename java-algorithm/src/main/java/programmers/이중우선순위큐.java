bbd06a630dabb4e45ad322f739533ef5472010fd
JINSAN NOH <zingoworks@gmail.com>
2019-10-14 00:05:52 +0900

    191014 이중우선순위큐

---
+++ java-algorithm/src/main/java/programmers/이중우선순위큐.java
@@ -0,0 +1,40 @@
+package programmers;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class 이중우선순위큐 {
+
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> maxPriorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minPriorityQueue = new PriorityQueue<>();
+
+        for(String operation : operations) {
+            int value = Integer.parseInt(operation.split(" ")[1]);
+
+            if(operation.startsWith("I")) {
+                maxPriorityQueue.add(value);
+                minPriorityQueue.add(value);
+            } else {
+                if(!maxPriorityQueue.isEmpty()) {
+                    if(value == 1) {
+                        int max = maxPriorityQueue.peek();
+                        maxPriorityQueue.remove(max);
+                        minPriorityQueue.remove(max);
+                    } else {
+                        int min = minPriorityQueue.peek();
+                        maxPriorityQueue.remove(min);
+                        minPriorityQueue.remove(min);
+                    }
+                }
+            }
+        }
+
+        return new int[]
+                {
+                        maxPriorityQueue.isEmpty() ? 0 : maxPriorityQueue.poll(),
+                        minPriorityQueue.isEmpty() ? 0 : minPriorityQueue.poll()
+                };
+    }
+
+}
