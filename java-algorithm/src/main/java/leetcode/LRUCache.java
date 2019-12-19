9ee1de365860e4f049f86d668273bb6f49ea5569
JINSAN NOH <zingoworks@gmail.com>
2019-12-19 23:59:46 +0900

    191219 LRUCache 추가

--- java-algorithm/src/main/java/leetcode/LRUCache.java
+++ java-algorithm/src/main/java/leetcode/LRUCache.java
@@ -1,9 +1,10 @@
 package leetcode;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-class LRUCache {
+public class LRUCache {
 
     private Map<Integer, Integer> cache;
 
@@ -59,6 +60,91 @@ class LRUCacheWithBasicLinkedHashMap {
             cache.remove(cache.keySet().iterator().next());
         }
         cache.put(key, value);
+    }
+
+}
+
+class LRUCacheWithDoublyLinkedListAndHashMap {
+
+    private Map<Integer, DoublyLinkedNode> cache = new HashMap<>();
+    private DoublyLinkedNode head, tail;
+    private int capacity;
+
+    public LRUCacheWithDoublyLinkedListAndHashMap(int capacity) {
+        this.capacity = capacity;
+
+        this.head = new DoublyLinkedNode();
+        this.head.pre = null;
+
+        this.tail = new DoublyLinkedNode();
+        this.tail.post = null;
+
+        this.head.post = tail;
+        this.tail.pre = head;
+    }
+
+    public int get(int key) {
+        if(cache.containsKey(key)) {
+            DoublyLinkedNode node = cache.get(key);
+            moveToHead(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if(cache.containsKey(key)) {
+            cache.get(key).value = value;
+            moveToHead(cache.get(key));
+        } else {
+            if(cache.size() == capacity) {
+                cache.remove(popTail().key);
+            }
+
+            DoublyLinkedNode newNode = new DoublyLinkedNode(key, value);
+            add(newNode);
+            cache.put(key, newNode);
+        }
+    }
+
+    class DoublyLinkedNode {
+        int key;
+        int value;
+        DoublyLinkedNode pre, post;
+
+        public DoublyLinkedNode() {}
+
+        public DoublyLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void add(DoublyLinkedNode node) {
+        node.pre = head;
+        node.post = head.post;
+
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    private void remove(DoublyLinkedNode node) {
+        DoublyLinkedNode pre = node.pre;
+        DoublyLinkedNode post = node.post;
+
+        pre.post = post;
+        post.pre = pre;
+    }
+
+    private void moveToHead(DoublyLinkedNode node) {
+        remove(node);
+        add(node);
+    }
+
+    private DoublyLinkedNode popTail() {
+        DoublyLinkedNode node = tail.pre;
+        remove(node);
+        return node;
     }
 
 }
