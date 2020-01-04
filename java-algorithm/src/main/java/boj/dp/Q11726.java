f0bc5013c3e9b106d2a30d27e145c2b47659ba6f
JINSAN NOH <zingoworks@gmail.com>
2020-01-04 20:03:45 +0900

    boj dp

---
+++ java-algorithm/src/main/java/boj/dp/Q11726.java
@@ -0,0 +1,25 @@
+package boj.dp;
+
+import java.io.*;
+
+public class Q11726 {
+
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        bw.write(Integer.toString(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+
+}
