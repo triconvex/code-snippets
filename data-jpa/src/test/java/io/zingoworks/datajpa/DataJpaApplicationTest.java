ed5bfe9b772f4ffe556f9a921735d73e1a19d85a
zingoworks <zingoworks@gmail.com>
2020-03-30 22:57:01 +0900

    Init data-jpa module

---
+++ data-jpa/src/test/java/io/zingoworks/datajpa/DataJpaApplicationTest.java
@@ -0,0 +1,13 @@
+package io.zingoworks.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
