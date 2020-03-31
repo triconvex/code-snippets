6a474cce78894a61b72d291f123923e2825fece0
zingoworks <zingoworks@gmail.com>
2020-03-31 21:02:51 +0900

    예제 도메인 엔티티 구현

--- data-jpa/src/main/java/io/zingoworks/datajpa/entity/Member.java
+++ data-jpa/src/main/java/io/zingoworks/datajpa/entity/Member.java
@@ -1,26 +1,34 @@
 package io.zingoworks.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //Team과 같은 연관관계 속성은 포함안하는게...
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     //TODO JPA 표준스펙으로 디폴트 생성자(인자없는), 접근권한 protected까지 있어야 함, JPA의 프록싱을 위해!!!
     // 관련 레퍼런스 찾아보기
-    protected Member() {
-    }
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
