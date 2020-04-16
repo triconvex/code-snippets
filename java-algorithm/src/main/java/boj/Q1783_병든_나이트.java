cf5fe3c9f198ba502d690ac61fca93b77d24bd0a
zingoworks <zingoworks@gmail.com>
2020-04-16 22:31:43 +0900

    200416 Q1783 병든나이트

---
+++ java-algorithm/src/main/java/boj/Q1783_병든_나이트.java
@@ -0,0 +1,33 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1783_병든_나이트 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
+
+        System.out.println(getGreatestCount(N, M));
+    }
+
+    public static int getGreatestCount(int N, int M) {
+        if(N == 1) {
+            return 1;
+        }
+        if(N == 2) {
+            return Math.min(4, (M + 1) / 2);
+        }
+        if(M < 7) {
+            return Math.min(4, M);
+        }
+        return M - 2;
+    }
+
+}
